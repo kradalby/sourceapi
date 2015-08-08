a0cd2cc4cfefcb0368965d0af394c67c390bf450
Kristoffer Dalby <kradalby@kradalby.no>
2015-08-08 13:10:49 +0200

    started on test to check if all views responds json, cant test on :plane:

---
+++ tests/api_test.py
@@ -0,0 +1,40 @@
+import json
+import unittest
+
+import app as sourceapi
+
+
+class GsoTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = sourceapi.app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_logic(self):
+        self.assertEqual(1, 1)
+
+    def test_index_render(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_json_response_for_all_routes_v1(self):
+        ROUTES = (
+            '/api/v1/all/',
+            '/api/v1/serverinfo',
+            '/api/v1/playerinfo',
+            '/api/v1/ping',
+            '/api/v1/rules',
+        )
+
+        test_server = 'z.fap.no:27015'
+
+        for route in ROUTES:
+            response = self.app.post(route, json.dumps([test_server]))
+            self.assertEqual(response.status_code, 200)
+            data = json.loads(response.content)
+
+
+if __name__ == '__main__':
+    unittest.main()
