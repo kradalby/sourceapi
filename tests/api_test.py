380dde4c5a6025a3b52c1c876aaedd24ee2abb49
Kristoffer Dalby <kradalby@kradalby.no>
2015-12-23 15:37:35 +0100

    add some tests

--- tests/api_test.py
+++ tests/api_test.py
@@ -3,6 +3,15 @@ import unittest
 
 import app as sourceapi
 
+WORKING_SERVER = '81.166.125.20:21300'
+NOT_WORKING_SERVER = 'fap.no:1337'
+
+
+def create_payload(server):
+    return dict(
+        data=server
+    )
+
 
 class SourceAPITestCase(unittest.TestCase):
 
@@ -16,8 +25,72 @@ class SourceAPITestCase(unittest.TestCase):
         self.assertEqual(1, 1)
 
     def fail(self):
-        self.assertEqual(1,2)
+        self.assertEqual(1, 2)
 
+    def test_json_response_for_all_routes_v1_with_working_server(self):
+        routes = (
+            '/api/v1/all',
+            '/api/v1/serverinfo',
+            '/api/v1/playerinfo',
+            '/api/v1/ping',
+            '/api/v1/rules',
+        )
+
+        payload = create_payload(WORKING_SERVER)
+
+        for route in routes:
+            response = self.app.post(
+                route,
+                data=json.dumps(payload),
+                content_type='application/json'
+            )
+            self.assertEqual(response.status_code, 200)
+            data = json.loads(response.data.decode('utf-8'))
+            self.assertEqual(data['status'], 'success')
+
+    def test_json_response_for_all_routes_v1_with_not_working_server(self):
+        routes = (
+            '/api/v1/all',
+            '/api/v1/serverinfo',
+            '/api/v1/playerinfo',
+            '/api/v1/ping',
+            '/api/v1/rules',
+        )
+
+        payload = create_payload(NOT_WORKING_SERVER)
+
+        for route in routes:
+            response = self.app.post(
+                route,
+                data=json.dumps(payload),
+                content_type='application/json'
+            )
+            self.assertEqual(response.status_code, 200)
+            data = json.loads(response.data.decode('utf-8'))
+            self.assertEqual(data['status'], 'error')
+            self.assertEqual(data['data'], 'The server did not respond')
+
+    def test_json_response_for_all_routes_v1_with_missing_data(self):
+        routes = (
+            '/api/v1/all',
+            '/api/v1/serverinfo',
+            '/api/v1/playerinfo',
+            '/api/v1/ping',
+            '/api/v1/rules',
+        )
+
+        payload = create_payload(NOT_WORKING_SERVER)
+
+        for route in routes:
+            response = self.app.post(
+                route,
+                data=json.dumps({'dat': 'derp'}),
+                content_type='application/json'
+            )
+            self.assertEqual(response.status_code, 200)
+            data = json.loads(response.data.decode('utf-8'))
+            self.assertEqual(data['status'], 'error')
+            self.assertEqual(data['data'], 'Missing data field')
 
 if __name__ == '__main__':
     unittest.main()
