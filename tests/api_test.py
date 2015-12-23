ee4e86ce1ba0f52f44a941ab38174122f096dbd2
Kristoffer Dalby <kradalby@kradalby.no>
2015-12-23 14:49:44 +0100

    updated documentation

--- tests/api_test.py
+++ tests/api_test.py
@@ -4,7 +4,7 @@ import unittest
 import app as sourceapi
 
 
-class GsoTestCase(unittest.TestCase):
+class SourceAPITestCase(unittest.TestCase):
 
     def setUp(self):
         self.app = sourceapi.app.test_client()
@@ -17,24 +17,6 @@ class GsoTestCase(unittest.TestCase):
 
     def fail(self):
         self.assertEqual(1,2)
-
-#    def test_json_response_for_all_routes_v1(self):
-#        routes = (
-#            '/api/v1/all/',
-#            '/api/v1/serverinfo',
-#            '/api/v1/playerinfo',
-#            '/api/v1/ping',
-#            '/api/v1/rules',
-#        )
-#
-#        test_server = '[["z.fap.no", 27015]]'
-#
-#        for route in routes:
-#            response = self.app.post(route, data=dict(test_server))
-#            print(response)
-#            print(dir(response))
-#            self.assertEqual(response.status_code, 200)
-#            data = json.loads(response.content)
 
 
 if __name__ == '__main__':
