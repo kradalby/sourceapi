ca011fbe6a5f3d97728e7fe82adcd61f5dbacf50
Kristoffer Dalby <kradalby@kradalby.no>
2015-12-28 19:47:04 +0100

    moved errors to its own file

---
+++ errors.py
@@ -0,0 +1,23 @@
+MISSING_DATA = {
+    'status': 'error',
+    'data': 'Missing data field',
+    'type': 'MD'
+}
+
+NO_RESPONSE = {
+    'status': 'error',
+    'data': 'The server did not respond',
+    'type': 'NR'
+}
+
+NOT_VALID_JSON = {
+    'status': 'error',
+    'data': 'The JSON is not valid',
+    'type': 'NVJ'
+}
+
+ENDPOINT_NOT_FOUND = {
+    'status': 'error',
+    'data': 'Endpoint not found',
+    'type': 'ENF'
+}
